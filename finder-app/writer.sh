0f60e861a6f6e256e5f66de8fe03df50bebfbd95
GiangNguyen110 <129675729+GiangNguyen110@users.noreply.github.com>
2023-08-11 17:14:10 +0700

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Shell script for Assignment 01.
+# Shell script finder-app/writer.sh of Assignment 01.
 # It accepts the following arguments:
 #   the first argument is a full path to a file (including filename) on the filesystem, referred to below as writefile;
-#   he second argument is a text string which will be written within this file, referred to below as writestr.
+#   the second argument is a text string which will be written within this file, referred to below as writestr.
 # It creates a new file with name and path writefile with content writestr, overwriting any existing file and creating 
 # the path if it doesn’t exist. Exits with value 1 and error print statement if the file could not be created.
 # Author: Nguyen Thu Giang
